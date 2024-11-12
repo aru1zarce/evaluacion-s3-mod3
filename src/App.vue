95be95db34b30b3a94145f157f3d75fa51ffc4b8
aru1arce <alex.stgo@gmail.com>
2024-11-12 03:05:48 -0300

    Evaluacion 3 del modulo 6

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header">
+      Tienda Bit-Game
+    </div>
+    <div class="content">
+      <div class="welcome">
+        <h1>¡Bienvenido!</h1>
+        <p>A continuación encontrarás la lista de los juegos que prefieres</p>
+      </div>
+      <GameList :games="games" @remove-game="removeGame" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GameList from './components/GameList.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GameList
+  },
+  data() {
+    return {
+      games: ["Arena of valor", "Pokemon Unite", "Donkey Kong", "Heroes of the storm", "Valorant"]
+    };
+  },
+  methods: {
+    removeGame(index) {
+      this.games.splice(index, 1);
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: #f4f4f4;
+}
+.header {
+  background-color: #333;
+  color: white;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 10px 0;
+  font-size: 24px;
+}
+.content {
+  margin: 20px;
+}
+.welcome {
+  background-color: #e0f7fa;
+  padding: 20px;
+  border-radius: 5px;
+  margin-bottom: 20px;
+}
+.welcome h1 {
+  margin: 0;
+  font-size: 24px;
+}
+.welcome p {
+  margin: 10px 0 0;
+  font-size: 16px;
 }
 </style>
---
+++ src/components/GameList.vue
@@ -0,0 +1,62 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Juego</th>
+        <th>Eliminar juego de tu lista</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(game, index) in games" :key="index">
+        <td>{{ index }}</td>
+        <td>{{ game }}</td>
+        <td><button class="btn-eliminar" @click="removeGame(index)">eliminar</button></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'GameList',
+  props: {
+    games: Array
+  },
+  methods: {
+    removeGame(index) {
+      this.$emit('remove-game', index);
+    }
+  }
+};
+</script>
+
+<style>
+table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 20px;
+}
+table, th, td {
+  border: 1px solid #333;
+}
+th, td {
+  padding: 10px;
+  text-align: center;
+}
+th {
+  background-color: #333;
+  color: white;
+}
+.btn-eliminar {
+  background-color: #ff4d4d;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  cursor: pointer;
+  border-radius: 3px;
+}
+.btn-eliminar:hover {
+  background-color: #ff1a1a;
+}
+</style>
